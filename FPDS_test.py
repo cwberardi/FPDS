46718e7ac181f4b7e8706263396c6ceaabe71291
cwberardi <cberardi@mit.edu>
2016-04-12 10:38:46 -0400

    Initial FPDS unit test script

---
+++ FPDS_test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 11 12:54:43 2016
+
+@author: Chris
+"""
+
+def tagtest(root, num):
+    test = []
+    for kid in root.iterchildren():
+        test.append(kid.tag)
+        
+    assert len(set(test))==num
+    
+def lentest(df, root):
+    assert len(df)==len(root.getchildren())-1
