43c5b26001acca049b578a8e81d67f127479c841
Chris Berardi <cberardi@mit.edu>
2016-04-14 15:45:32 -0400

    Tested parser against AF data from FY05-FY15 . . . FY07 and FY04 were excluded because the xml is corrupt.  Added a function to make the nested getattr easier.

--- FPDS parse.py
+++ FPDS parse.py
@@ -8,27 +8,49 @@ Created on Tue Apr  5 11:15:37 2016
 from lxml import objectify
 import pandas as pd
 import numpy as np
-from FPDS_test import tagtest
-pd.set_option('float_format',"%.2f")
+import string
+from FPDS_test import get_xmlList, tagtest
+pd.set_option('display.float_format', lambda x:'%f'%x)
+
+HDF5path = r'C:\Users\Chris\Documents\MIT\Dissertation\FPDS\Data\FPDS.h5'
 #%%
 def FPDSxmlparse(path, default=np.nan, tags = 2):
     '''
-    Function sorts through each child of the xml root, except for the 'count' child, and extracts relevent xml tags using the python getattr method, which 
-    returns the respective data if the specificied dependenet path is present or returns a default response if not present.
+    Function sorts through each child of the xml root, except for the 'count' child.
        input: 
            path [string] - valid file path to xml archive data
            default[optional] - value returned if dependent path is not in child
            tags[int] - value used to check the number of xml tags in a given archive
        return: pandas dataframe with each xml child as a row
     '''
-    assert type(path)   ==str    
-    data                = {}
-    default             = default
+    assert type(path)   ==str, 'path is not a string'    
+    assert type(tags)   ==int, 'Number of tags is not an integer'
+    
+    def childhandlr(child, path, dtype, default):
+        '''
+        extracts relevent xml tags using the python getattr method, which returns the respective data if the specificied 
+        dependenet path is present or returns a default response if not present.
+        input:
+            child [objectify.ObjectifiedElement] - child for which the decendent path is desired
+            path [string] - decendent path
+            dtype [string] - data type for xml tag [options: 'text' or 'pyval']
+            default [optional] - value used to return if xml tag is not present
+        return: xml string, float, or integer depending on specified data type
+        '''
+        assert type(path)   ==str, 'path is not a string'
+        assert type(dtype)  ==str, 'dtype is not a string'
+        #TODO: need to figure out why the below assertion is throwing an error
+        #assert type(child)  ==objectify.ObjectifiedElement, 'Child is not an objectify.ObjectifiedElement'
+        
+        return getattr(getattr(child, path, default), dtype, default)      
     
     parsed = objectify.parse(path)
     root = parsed.getroot()
     tagtest(root, tags)
+
+    print('File parsed.  Now extracting '+ str(root.countchildren()) + ' xml elements')    
     
+    data                = {}  
     for i, kid in enumerate(root.getchildren()):
         # 'count' child does not contain valid contract data, consequently it is skipped
         if kid.tag == '{http://www.fpdsng.com/FPDS}count':
@@ -45,62 +67,63 @@ def FPDSxmlparse(path, default=np.nan, tags = 2):
                 piidIDV = None
                 modNumIDV = None
             
-            data[i]={'modNum' : modNum,
-                       'piid' : piid,
-                       'piidIDV' : piidIDV,
-                       'modNumIDV': modNumIDV,
-                       'agencyID' : kid.awardID.awardContractID.agencyID.text, 
-                       'transNum': kid.awardID.awardContractID.transactionNumber.text,
-                       'comp_type': getattr(getattr(kid.competition, 'extentCompeted', default), 'text', default),
-                       'vendorName': kid.vendor.vendorHeader.vendorName.text,
-                       'vendorLocationState' : getattr(getattr(kid.vendor.vendorSiteDetails.vendorLocation, 'state', default), 'text', default),
-                       'vendorLocationZip' : getattr(getattr(kid.vendor.vendorSiteDetails.vendorLocation, 'ZIPCode', default), 'pyval', default),
-                       'vendorLocaitonCongDist' : getattr(getattr(kid.vendor.vendorSiteDetails.vendorLocation, 'congressionalDistrictCode', default), 'pyval', default),
-                       'vendorCOSmallBussinesDetermination': kid.vendor.contractingOfficerBusinessSizeDetermination.text,
-                       'placeOfPerformanceState': getattr(getattr(kid.placeOfPerformance.principalPlaceOfPerformance, 'stateCode', default),'text', default),
-                       'placeofPerformanceZip': getattr(getattr(kid.placeOfPerformance, 'placeOfPerformanceZIPCode', default), 'pyval', default),
-                       'placeofPerformanceCongDist' : getattr(getattr(kid.placeOfPerformance, 'placeOfPerformanceCongressionalDistrict', default),'pyval', default),
-                       'vendorDUNS': kid.vendor.vendorSiteDetails.vendorDUNSInformation.DUNSNumber.pyval,
-                       'solicitation_procedures': getattr(getattr(kid.competition, 'solicitationProcedures', default),'text', default),
-                       'descriptionOfContractRequirement': getattr(getattr(kid.contractData, 'descriptionOfContractRequirement', default),'text', default),
-                       'typ_set_aside': getattr(getattr(kid.competition, 'typeOfSetAside', default), 'text', default),
-                       'statuteExcpToFairOp': getattr(getattr(kid.competition, 'statutoryExceptionToFairOpportunity', default),'text', default),
+            data[i]={'modNum' :                 modNum,
+                       'piid' :                 piid,
+                       'piidIDV' :              piidIDV,
+                       'modNumIDV':             modNumIDV,
+                       'agencyID' :             kid.awardID.awardContractID.agencyID.text, 
+                       'transNum':              kid.awardID.awardContractID.transactionNumber.text,
+                       'comp_type':             childhandlr(kid.competition, 'extentCompeted', 'text', default),
+                       'vendorName':            childhandlr(getattr(kid.vendor, 'vendorHeader', default), 'vendorName', 'text', default),
+                       'vendorLocState' :       childhandlr(getattr(kid.vendor.vendorSiteDetails,'vendorLocation',default), 'state', 'text', default),
+                       'vendorLocZip' :         childhandlr(getattr(kid.vendor.vendorSiteDetails, 'vendorLocation', default), 'ZIPCode', 'pyval', default),
+                       'vendorLocCongDist':     childhandlr(getattr(kid.vendor.vendorSiteDetails, 'vendorLocation', default), 'congressionalDistrictCode', 'pyval', default),
+                       'vendorCOSmallBusDeter': childhandlr(kid.vendor, 'contractingOfficerBusinessSizeDetermination', 'text', default),
+                       'placeOfPerfState':      childhandlr(kid.placeOfPerformance.principalPlaceOfPerformance, 'stateCode','text', default),
+                       'placeofPerfZip':        childhandlr(kid.placeOfPerformance, 'placeOfPerformanceZIPCode', 'pyval', default),
+                       'placeofPerfCongDist' :  childhandlr(kid.placeOfPerformance, 'placeOfPerformanceCongressionalDistrict', 'pyval', default),
+                       'vendorDUNS':            childhandlr(kid.vendor.vendorSiteDetails.vendorDUNSInformation,'DUNSNumber','pyval', default),
+                       'solprocedures':         childhandlr(kid.competition, 'solicitationProcedures','text', default),
+                       'descripOfContReqS':     childhandlr(kid.contractData, 'descriptionOfContractRequirement','text', default),
+                       'typ_set_aside':         childhandlr(kid.competition, 'typeOfSetAside', 'text', default),
+                       'statuteExcpToFairOp':   childhandlr(kid.competition, 'statutoryExceptionToFairOpportunity','text', default),
     #                   'evaluated_Preference': getattr(kid.competition, 'evaluated_Preference', default),  #Dead attribute, no values in any of Other DoD entries
-                       'obligatedAmount': kid.dollarValues.obligatedAmount.pyval,
-                       'baseAndExercisedOptionsValue': kid.dollarValues.baseAndExercisedOptionsValue.pyval,
-                       'baseAndAllOptionsValue': kid.dollarValues.baseAndAllOptionsValue.pyval,
-                       'numberOfOffersReceived': kid.competition.numberOfOffersReceived.pyval,
-                       'fedBizOpps': getattr(getattr(kid.competition, 'fedBizOpps', default),'text', default),
-                       'signedDate':kid.relevantContractDates.signedDate.text,
-                       'effectiveDate': kid.relevantContractDates.effectiveDate.text,
-                       'currentCompletionDate':kid.relevantContractDates.currentCompletionDate.text,
-                       'ultimateCompletionDate':kid.relevantContractDates.ultimateCompletionDate.text,
-                       'contractingOfficeAgencyID': kid.purchaserInformation.contractingOfficeAgencyID.text,
-                       'contractingOfficeID': kid.purchaserInformation.contractingOfficeID.text,
-                       'forProfit': getattr(getattr(kid.vendor.vendorSiteDetails.vendorOrganizationFactors.profitStructure, 'isForProfitOrganization', default),'text', default),
-                       'fundingRequestingAgencyID': kid.purchaserInformation.fundingRequestingAgencyID.pyval,
-                       'fundingRequestingOfficeID': kid.purchaserInformation.fundingRequestingOfficeID.pyval,
-                       'multiYearContract': getattr(getattr(kid.contractData, 'multiYearContract', default),'text', default),
-                       'GFE-GFP': kid.contractData['GFE-GFP'].text,
-                       'typeOfContractPricing' : getattr(getattr(kid.contractData, 'typeOfContractPricing', default),'text', default),
-                       'contractActionType': kid.contractData.contractActionType.text,
-                       'costOrPricingData': getattr(getattr(kid.contractData, 'costOrPricingData', default), 'text', default), 
-                       'undefinitizedAction': kid.contractData.undefinitizedAction.text,
-                       'performanceBasedServiceContract': getattr(getattr(kid.contractData, 'performanceBasedServiceContract', default),'text', default),
-                       'contractFinancing': getattr(getattr(kid.contractData, 'contractFinancing', default), 'text', default),
-                       'foreignFunding': kid.purchaserInformation.foreignFunding.text,
-                       'productOrServiceCode': kid.productOrServiceInformation.productOrServiceCode.text,
-                       'contractBundling': getattr(getattr(kid.productOrServiceInformation, 'contractBundling', default),'text', default),
-                       'claimantProgramCode': getattr(getattr(kid.productOrServiceInformation, 'claimantProgramCode', default), 'text', default),
-                       'principalNAICSCode': getattr(getattr(kid.productOrServiceInformation, 'principalNAICSCode', default), 'pyval', default),
-                       'systemEquipmentCode': getattr(getattr(kid.productOrServiceInformation, 'systemEquipmentCode', default),'text', default),
-                       'itCommercialItemCategory': getattr(getattr(kid.productOrServiceInformation, 'informationTechnologyCommercialItemCategory', default), 'text', default),
-                       'reason_not_competed': getattr(getattr(kid.competition, 'reasonNotCompeted', default), 'text', default)}
+                       'obligatedAmount':       childhandlr(kid.dollarValues, 'obligatedAmount', 'pyval',default),
+                       'base/ExerOptValue':     childhandlr(kid.dollarValues, 'baseAndExercisedOptionsValue', 'pyval' ,default),
+                       'base/AllOptionsValue':  childhandlr(kid.dollarValues, 'baseAndAllOptionsValue', 'pyval',default),
+                       'numOfOffersReceived':   childhandlr(kid.competition, 'numberOfOffersReceived', 'pyval', default),
+                       'fedBizOpps':            childhandlr(kid.competition,'fedBizOpps','text', default),
+                       'signedDate':            childhandlr(kid.relevantContractDates, 'signedDate','text',default),
+                       'effectiveDate':         childhandlr(kid.relevantContractDates,'effectiveDate','text',default),
+                       'currentCompletionDate': childhandlr(kid.relevantContractDates, 'currentCompletionDate', 'text',default),
+                       'ultimateCompletionDate':childhandlr(kid.relevantContractDates, 'ultimateCompletionDate','text', default),
+                       'k_OfficeAgencyID':      childhandlr(kid.purchaserInformation, 'contractingOfficeAgencyID','text', default),
+                       'k_OfficeID':            childhandlr(kid.purchaserInformation, 'contractingOfficeID','text',default),
+                       'forProfit':             childhandlr(kid.vendor.vendorSiteDetails.vendorOrganizationFactors.profitStructure, 'isForProfitOrganization','text', default),
+                       'fundingReqAgencyID':    childhandlr(kid.purchaserInformation, 'fundingRequestingAgencyID', 'pyval', default),
+                       'fundingReqOfficeID':    childhandlr(kid.purchaserInformation, 'fundingRequestingOfficeID', 'pyval',default),
+                       'multiYearContract':     childhandlr(kid.contractData, 'multiYearContract','text', default),
+                       'GFE-GFP':               kid.contractData['GFE-GFP'].text,
+                       'typeOfContractPricing': childhandlr(kid.contractData, 'typeOfContractPricing','text', default),
+                       'contractActionType':    childhandlr(kid.contractData, 'contractActionType', 'text',default),
+                       'costOrPricingData':     childhandlr(kid.contractData, 'costOrPricingData','text', default), 
+                       'undefinitizedAction':   childhandlr(kid.contractData, 'undefinitizedAction','text',default),
+                       'perfBasedServiceK':     childhandlr(kid.contractData, 'performanceBasedServiceContract','text', default),
+                       'contractFinancing':     childhandlr(kid.contractData, 'contractFinancing', 'text', default),
+                       'foreignFunding':        childhandlr(kid.purchaserInformation, 'foreignFunding','text', default),
+                       'productOrServiceCode':  childhandlr(kid.productOrServiceInformation, 'productOrServiceCode', 'text',default),
+                       'contractBundling':      childhandlr(kid.productOrServiceInformation, 'contractBundling','text', default),
+                       'claimantProgramCode':   childhandlr(kid.productOrServiceInformation, 'claimantProgramCode','text', default),
+                       'principalNAICSCode':    childhandlr(kid.productOrServiceInformation, 'principalNAICSCode','pyval', default),
+                       'systemEquipmentCode':   childhandlr(kid.productOrServiceInformation, 'systemEquipmentCode','text', default),
+                       'itCommercialItemCat':   childhandlr(kid.productOrServiceInformation, 'informationTechnologyCommercialItemCategory','text', default),
+                       'reason_not_competed':   childhandlr(kid.competition, 'reasonNotCompeted','text', default)
+                       }
                        
     df= pd.DataFrame.from_dict(data, orient = 'index')
     
     # Convert date columns into datetime format
-    for x in ['ultimateCompletionDate','signedDate', 'currentCompletionDate', 'effectiveDate']: df[x] = pd.to_datetime(df[x])
+    for x in ['ultimateCompletionDate','signedDate', 'currentCompletionDate', 'effectiveDate']: df[x] = pd.to_datetime(df[x], errors = 'coerce')
         
     # Check to make sure length of dataframe matches children in root 
     assert len(df)==len(root.getchildren())-1
@@ -130,7 +153,11 @@ def FPDSstoreh5(path, key, df):
     store = pd.HDFStore(path)
     
     if storeName in store.keys():
-        assert storeName in store.keys(), 'Key already in HDF5 Store'  
+        print('****Key '+ key + ' already in HDF5 Store***')
+        rewrite = input('Delete old key/dataframe and add new key/dataframe? (y/n): ')
+        if rewrite.lower() == 'y':
+            del store[key]
+            store[key] = df
     else:
         store[key] = df
         
@@ -138,7 +165,26 @@ def FPDSstoreh5(path, key, df):
     store.close()
     
 #%%
-pathr = r'C:\Users\Chris\Downloads\DOD-OTHER_DOD-DEPT-10012014TO09302015-Archive\OTHER_DOD_AGENCIES-AWARD.xml'
-paths = r'C:\Users\Chris\Documents\MIT\Dissertation\FPDS\Data\FPDS.h5'
-FPDSstoreh5(paths, 'Other_DoD_FY14', FPDSxmlparse(pathr))
+
+for x in get_xmlList(r'C:\Users\Chris\Downloads\Air Force'):    
+    key = x.split('\\')[-1].split('.')[0].lstrip(string.digits+'-').lower().replace('-','_')
+    FPDSstoreh5(HDF5path, key, FPDSxmlparse(x))
+
+#%%%
+def makeCat(columns, df):
+    for x in columns:
+        df[x] = df[x].astype('category')
+        
+store = pd.HDFStore(HDF5path, mode='r')
+afDF = pd.DataFrame()
+for x in store.keys():
+    afDF = pd.concat((afDF, store[x]), ignore_index = True)
+store.close()
+
+catCols = ['GFE-GFP', 'comp_type', 'contractActionType','contractBundling','contractFinancing','costOrPricingData', 'typ_set_aside', 'typeOfContractPricing','undefinitizedAction',
+           'vendorCOSmallBusDeter','statuteExcpToFairOp', 'solprocedures', 'reason_not_competed', 'agencyID', 'claimantProgramCode', 'fedBizOpps','foreignFunding',
+           'fundingReqAgencyID','fundingReqOfficeID', 'itCommercialItemCat', 'multiYearContract']
+
+makeCat(catCols, afDF)
+#%%
 #df.isnull().sum().sort_values(ascending=False).div(len(df))
